6192993e591d881a75fbfbd8add5b6a5ed8ecf70
RobbieConnor <me@robbieconnor.com>
2021-11-03 14:52:11 +0000

    Initial Commit.
    Adds initial files, alongside initial movement system and first Bug AI

---
+++ Shrink Bug/Assets/BugBehaviour.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BugBehaviour : MonoBehaviour
+{
+    public float LRMovementSpeed;
+    public float DownMovementSpeed;
+    public int health;
+    public int damage;
+    public bool goingRight = false;
+    public GameObject generalStats;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 oldPos = transform.position;
+        Vector3 newPos = new Vector3(oldPos.x, oldPos.y, oldPos.z);
+        if (goingRight) {
+            newPos += new Vector3(LRMovementSpeed, 0, 0) * Time.deltaTime;
+        } else {
+            newPos += new Vector3(-LRMovementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        newPos += new Vector3(0, -DownMovementSpeed, 0) * Time.deltaTime;
+
+        transform.position = newPos;
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        if (col.gameObject.tag == "Wall") {
+            goingRight = !goingRight;
+        }
+
+        if (col.gameObject.tag == "DamageHitbox") {
+            perishAndDealDamage();
+        }
+    }
+
+    void perishAndDealDamage() {
+        generalStats.GetComponent<GeneralStatsBehaviour>().playerHealth -= damage;
+        Destroy(this.gameObject);
+    }
+}
---
+++ Shrink Bug/Assets/PlayerMovementBehaviour.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehaviour : MonoBehaviour
+{
+
+    public float movementSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 currentPos = transform.position;
+        Vector3 newPos = new Vector3(currentPos.x, currentPos.y, currentPos.z);
+        if (Input.GetKey(KeyCode.W)) {
+            newPos += new Vector3(0, movementSpeed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            newPos += new Vector3(0, -movementSpeed, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            newPos += new Vector3(-movementSpeed, 0, 0) * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            newPos += new Vector3(movementSpeed, 0, 0) * Time.deltaTime;
+        }
+
+        transform.position = newPos;
+    }
+}
