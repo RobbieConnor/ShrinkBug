2a5cfac9bc6cc50c18367f501a36b7cd8f294287
RobbieConnor <me@robbieconnor.com>
2021-11-04 23:55:47 +0000

    Added Power-Ups, randomly spawning at 10% when killing a bug

--- Shrink Bug/Assets/EnemyBehaviour.cs
+++ Shrink Bug/Assets/EnemyBehaviour.cs
@@ -10,6 +10,8 @@ public class EnemyBehaviour : MonoBehaviour
     public BugSpawnBehaviour bugSpawner;
 
     public TextControllerBehaviour textController;
+
+    public GameObject basicDrop;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,6 +36,24 @@ public class EnemyBehaviour : MonoBehaviour
 
     void perish() {
         Destroy(this.gameObject);
+
+        var rng = new System.Random();
+
+        var spawnItem = rng.Next(100);
+        if (spawnItem >= 90) {
+
+            var itemDropType = rng.Next(4);
+
+            GameObject drop = Instantiate(basicDrop, transform.position, Quaternion.identity);
+
+            if (itemDropType == 1) {
+                drop.GetComponent<PowerUpBehaviour>().effectToApply = "atkSpeedUp";
+            } else if (itemDropType == 2) {
+                drop.GetComponent<PowerUpBehaviour>().effectToApply = "bulletSpeedUp";
+            } else {
+                drop.GetComponent<PowerUpBehaviour>().effectToApply = "moveUp";
+            }
+        }
         bugSpawner.checkForRemainingEnemies();
     }
 }
---
+++ Shrink Bug/Assets/PowerUpBehaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpBehaviour : MonoBehaviour
+{
+
+    public GameObject player;
+
+    public string effectToApply;
+
+    public GameObject spriteGameObject;
+
+    public Sprite moveUpSprite;
+    public Sprite bulletSpeedUpSprite;
+    public Sprite atkSpeedUpSprite;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("PlayerHitbox");
+
+        switch(effectToApply) {
+            case "atkSpeedUp":
+                spriteGameObject.GetComponent<SpriteRenderer>().sprite = atkSpeedUpSprite;
+            break;
+            case "bulletSpeedUp":
+                spriteGameObject.GetComponent<SpriteRenderer>().sprite = bulletSpeedUpSprite;
+            break;
+            case "moveUp":
+                spriteGameObject.GetComponent<SpriteRenderer>().sprite = moveUpSprite;
+            break;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col) {
+        Debug.Log("COLLISION");
+        if (col.gameObject == player) {
+            applyEffect();
+            Destroy(this.gameObject);
+        }
+    }
+
+    void applyEffect() {
+        switch (effectToApply) {
+            case "atkSpeedUp":
+                player.GetComponent<PlayerShootBehaviour>().gunCooldownMax -= (player.GetComponent<PlayerShootBehaviour>().gunCooldownMax * 0.1f);
+            break;
+            case "bulletSpeedUp":
+                player.GetComponent<PlayerShootBehaviour>().bulletSpeed += (player.GetComponent<PlayerShootBehaviour>().bulletSpeed * 0.1f);
+            break;
+            case "moveUp":
+                player.GetComponent<PlayerMovementBehaviour>().movementSpeed += (player.GetComponent<PlayerMovementBehaviour>().movementSpeed * 0.1f);
+            break;
+        }
+    }
+}
